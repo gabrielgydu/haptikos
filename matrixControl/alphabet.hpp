5f803505f98c70c5fc8b1d27d1ed83fd01f5f974
Gabriel <gabrielsouza@me.com>
2016-09-23 01:46:19 -0300

    commit

---
+++ matrixControl/alphabet.hpp
@@ -0,0 +1,793 @@
+#ifndef ALPHABET_HPP_INCLUDED
+#define ALPHABET_HPP_INCLUDED
+
+
+
+#endif // ALPHABET_HPP_INCLUDED
+
+
+
+
+
+void letraA(){
+    motorOnTime(100, 100);
+    motorOnTime2(89, 90, 100);
+    motorOnTime(79, 100);
+    motorOnTime2(68, 69, 100);
+    motorOnTime(58, 100);
+    motorOnTime2(47, 48, 100);
+    motorOnTime(37, 100);
+    motorOnTime2(26, 27, 100);
+    motorOnTime(16, 100);
+    motorOnTime(15, 100);
+    motorOnTime2(24, 25, 100);
+    motorOnTime(34, 100);
+    motorOnTime2(43, 44, 100);
+    motorOnTime(53, 100);
+    motorOnTime2(62, 63, 100);
+    motorOnTime(72, 100);
+    motorOnTime2(81, 82, 100);
+    motorOnTime(91, 100);
+
+    motorOnTime(68, 100);
+    motorOnTime(67, 100);
+    motorOnTime(66, 100);
+    motorOnTime(65, 100);
+    motorOnTime(64, 100);
+    motorOnTime(63, 100);
+}
+
+void letraB(){
+    motorOnTime(8, 100);
+    motorOnTime(18, 100);
+    motorOnTime(28, 100);
+    motorOnTime(38, 100);
+    motorOnTime(48, 100);
+    motorOnTime(58, 100);
+    motorOnTime(68, 100);
+    motorOnTime(78, 100);
+    motorOnTime(88, 100);
+    motorOnTime(98, 100);
+
+    motorOnTime(8, 100);
+    motorOnTime(7, 100);
+    motorOnTime2(6, 16, 100);
+    motorOnTime(15, 100);
+    motorOnTime(25, 100);
+    motorOnTime(35, 100);
+    motorOnTime(46, 100);
+    motorOnTime(47, 100);
+    motorOnTime(57, 100);
+    motorOnTime(56, 100);
+    motorOnTime(55, 100);
+    motorOnTime(64, 100);
+    motorOnTime2(73, 74, 100);
+    motorOnTime(84, 100);
+    motorOnTime(95, 100);
+    motorOnTime(96, 100);
+    motorOnTime(97, 100);
+}
+
+void letraC(){
+    motorOnTime(22, 100);
+    motorOnTime2(12, 13, 100);
+    motorOnTime(4, 100);
+    motorOnTime(5, 100);
+    motorOnTime(6, 100);
+    motorOnTime2(7, 17, 100);
+    motorOnTime2(18, 28, 100);
+    motorOnTime(39, 100);
+    motorOnTime(49, 100);
+    motorOnTime(59, 100);
+    motorOnTime(69, 100);
+    motorOnTime2(78, 88, 100);
+    motorOnTime2(87, 97, 100);
+    motorOnTime(96, 100);
+    motorOnTime(95, 100);
+    motorOnTime(94, 100);
+    motorOnTime2(82,83, 100);
+    motorOnTime(72, 100);
+
+}
+
+void letraD(){
+    motorOnTime(8, 100);
+    motorOnTime(18, 100);
+    motorOnTime(28, 100);
+    motorOnTime(38, 100);
+    motorOnTime(48, 100);
+    motorOnTime(58, 100);
+    motorOnTime(68, 100);
+    motorOnTime(78, 100);
+    motorOnTime(88, 100);
+    motorOnTime(98, 100);
+
+    motorOnTime(8, 100);
+    motorOnTime(7, 100);
+    motorOnTime(6, 100);
+    motorOnTime(5, 100);
+    motorOnTime2(4, 14, 100);
+    motorOnTime2(13, 23, 100);
+    motorOnTime(32, 100);
+    motorOnTime(42, 100);
+    motorOnTime(52, 100);
+    motorOnTime(62, 100);
+    motorOnTime2(72, 73, 100);
+    motorOnTime2(83, 84, 100);
+    motorOnTime(95, 100);
+    motorOnTime(96, 100);
+    motorOnTime(97, 100);
+    motorOnTime(98, 100);
+
+}
+
+void letraE(){
+    motorOnTime(8, 100);
+    motorOnTime(18, 100);
+    motorOnTime(28, 100);
+    motorOnTime(38, 100);
+    motorOnTime(48, 100);
+    motorOnTime(58, 100);
+    motorOnTime(68, 100);
+    motorOnTime(78, 100);
+    motorOnTime(88, 100);
+    motorOnTime(98, 100);
+
+    motorOnTime(8, 100);
+    motorOnTime(7, 100);
+    motorOnTime(6, 100);
+    motorOnTime(5, 100);
+    motorOnTime(4, 100);
+
+    motorOnTime(48, 100);
+    motorOnTime(47, 100);
+    motorOnTime(46, 100);
+    motorOnTime(45, 100);
+    motorOnTime(44, 100);
+
+    motorOnTime(98, 100);
+    motorOnTime(97, 100);
+    motorOnTime(96, 100);
+    motorOnTime(95, 100);
+    motorOnTime(94, 100);
+
+
+
+
+}
+
+void letraF(){
+    motorOnTime(8, 100);
+    motorOnTime(18, 100);
+    motorOnTime(28, 100);
+    motorOnTime(38, 100);
+    motorOnTime(48, 100);
+    motorOnTime(58, 100);
+    motorOnTime(68, 100);
+    motorOnTime(78, 100);
+    motorOnTime(88, 100);
+    motorOnTime(98, 100);
+
+    motorOnTime(8, 100);
+    motorOnTime(7, 100);
+    motorOnTime(6, 100);
+    motorOnTime(5, 100);
+    motorOnTime(4, 100);
+
+    motorOnTime(48, 100);
+    motorOnTime(47, 100);
+    motorOnTime(46, 100);
+    motorOnTime(45, 100);
+    motorOnTime(44, 100);
+}
+
+void letraG(){
+    motorOnTime(12, 100);
+    motorOnTime2(3, 13, 100);
+    motorOnTime(4, 100);
+    motorOnTime(5, 100);
+    motorOnTime(6, 100);
+    motorOnTime(7, 100);
+    motorOnTime2(8, 18, 100);
+    motorOnTime2(19, 29, 100);
+    motorOnTime(40, 100);
+    motorOnTime(50, 100);
+    motorOnTime(60, 100);
+    motorOnTime(70, 100);
+    motorOnTime2(79, 89, 100);
+    motorOnTime2(88, 98, 100);
+    motorOnTime(87, 100);
+    motorOnTime(96, 100);
+    motorOnTime(95, 100);
+    motorOnTime(94, 100);
+    motorOnTime2(83, 93, 100);
+    motorOnTime2(72, 82, 100);
+    motorOnTime(61, 100);
+    motorOnTime(51, 100);
+    motorOnTime(52, 100);
+    motorOnTime(53, 100);
+    motorOnTime(54, 100);
+    motorOnTime(55, 100);
+    motorOnTime(56, 100);
+
+
+
+}
+
+void letraH(){
+    motorOnTime(8, 100);
+    motorOnTime(18, 100);
+    motorOnTime(28, 100);
+    motorOnTime(38, 100);
+    motorOnTime(48, 100);
+    motorOnTime(58, 100);
+    motorOnTime(68, 100);
+    motorOnTime(78, 100);
+    motorOnTime(88, 100);
+    motorOnTime(98, 100);
+
+    motorOnTime(3, 100);
+    motorOnTime(13, 100);
+    motorOnTime(23, 100);
+    motorOnTime(33, 100);
+    motorOnTime(43, 100);
+    motorOnTime(53, 100);
+    motorOnTime(63, 100);
+    motorOnTime(73, 100);
+    motorOnTime(83, 100);
+    motorOnTime(93, 100);
+
+    motorOnTime(48, 100);
+    motorOnTime(47, 100);
+    motorOnTime(46, 100);
+    motorOnTime(45, 100);
+    motorOnTime(44, 100);
+    motorOnTime(43, 100);
+
+}
+
+void letraI(){
+    motorOnTime2(5, 6, 100);
+    motorOnTime2(15, 16, 100);
+    motorOnTime2(25, 26, 100);
+    motorOnTime2(35, 36, 100);
+    motorOnTime2(45, 46, 100);
+    motorOnTime2(55, 56, 100);
+    motorOnTime2(65, 66, 100);
+    motorOnTime2(75, 76, 100);
+    motorOnTime2(85, 86, 100);
+    motorOnTime2(95, 96, 100);
+
+}
+
+void letraJ(){
+    motorOnTime(5, 100);
+    motorOnTime(15, 100);
+    motorOnTime(25, 100);
+    motorOnTime(35, 100);
+    motorOnTime(45, 100);
+    motorOnTime(55, 100);
+    motorOnTime(65, 100);
+    motorOnTime(75, 100);
+    motorOnTime(85, 100);
+    motorOnTime(96, 100);
+    motorOnTime(97, 100);
+    motorOnTime(88, 100);
+}
+
+void letraK(){
+    motorOnTime(8, 100);
+    motorOnTime(18, 100);
+    motorOnTime(28, 100);
+    motorOnTime(38, 100);
+    motorOnTime(48, 100);
+    motorOnTime(58, 100);
+    motorOnTime(68, 100);
+    motorOnTime(78, 100);
+    motorOnTime(88, 100);
+    motorOnTime(98, 100);
+
+    motorOnTime(3, 100);
+    motorOnTime(14, 100);
+    motorOnTime(25, 100);
+    motorOnTime(36, 100);
+    motorOnTime(47, 100);
+    motorOnTime(56, 100);
+    motorOnTime2(65, 75, 100);
+    motorOnTime(84, 100);
+    motorOnTime(93, 100);
+}
+
+void letraL(){
+    motorOnTime(8, 100);
+    motorOnTime(18, 100);
+    motorOnTime(28, 100);
+    motorOnTime(38, 100);
+    motorOnTime(48, 100);
+    motorOnTime(58, 100);
+    motorOnTime(68, 100);
+    motorOnTime(78, 100);
+    motorOnTime(88, 100);
+    motorOnTime(98, 100);
+
+    motorOnTime(97, 100);
+    motorOnTime(96, 100);
+    motorOnTime(95, 100);
+    motorOnTime(94, 100);
+
+}
+
+void letraM(){
+    motorOnTime(99, 100);
+    motorOnTime(89, 100);
+    motorOnTime(79, 100);
+    motorOnTime(69, 100);
+    motorOnTime(59, 100);
+    motorOnTime(49, 100);
+    motorOnTime(39, 100);
+    motorOnTime(29, 100);
+    motorOnTime(19, 100);
+    motorOnTime(9, 100);
+
+    motorOnTime2(18, 19, 100);
+    motorOnTime(28, 100);
+    motorOnTime2(37, 38, 100);
+    motorOnTime(47, 100);
+    motorOnTime2(56, 57, 100);
+    motorOnTime(66, 100);
+    motorOnTime(65, 100);
+    motorOnTime2(54, 55, 100);
+    motorOnTime(44, 100);
+    motorOnTime2(33, 34, 100);
+    motorOnTime(23, 100);
+    motorOnTime2(12, 13, 100);
+
+
+    motorOnTime(2, 100);
+    motorOnTime(12, 100);
+    motorOnTime(22, 100);
+    motorOnTime(32, 100);
+    motorOnTime(42, 100);
+    motorOnTime(52, 100);
+    motorOnTime(62, 100);
+    motorOnTime(72, 100);
+    motorOnTime(82, 100);
+    motorOnTime(92, 100);
+
+}
+
+void letraN(){
+
+    motorOnTime(100, 100);
+    motorOnTime(90, 100);
+    motorOnTime(80, 100);
+    motorOnTime(70, 100);
+    motorOnTime(60, 100);
+    motorOnTime(50, 100);
+    motorOnTime(40, 100);
+    motorOnTime(30, 100);
+    motorOnTime(20, 100);
+    motorOnTime(10, 100);
+
+    motorOnTime(19, 100);
+    motorOnTime(28, 100);
+    motorOnTime(37, 100);
+    motorOnTime(46, 100);
+    motorOnTime(55, 100);
+    motorOnTime(64, 100);
+    motorOnTime(73, 100);
+    motorOnTime(82, 100);
+
+    motorOnTime(91, 100);
+    motorOnTime(81, 100);
+    motorOnTime(71, 100);
+    motorOnTime(61, 100);
+    motorOnTime(51, 100);
+    motorOnTime(41, 100);
+    motorOnTime(31, 100);
+    motorOnTime(21, 100);
+    motorOnTime(11, 100);
+    motorOnTime(1, 100);
+
+}
+
+void letraO(){
+    motorOnTime(12, 100);
+    motorOnTime2(3, 13, 100);
+    motorOnTime(4, 100);
+    motorOnTime(5, 100);
+    motorOnTime(6, 100);
+    motorOnTime(7, 100);
+    motorOnTime2(8, 18, 100);
+    motorOnTime2(19, 29, 100);
+    motorOnTime(40, 100);
+    motorOnTime(50, 100);
+    motorOnTime(60, 100);
+    motorOnTime(70, 100);
+    motorOnTime2(79, 89, 100);
+    motorOnTime2(88, 98, 100);
+    motorOnTime(87, 100);
+    motorOnTime(96, 100);
+    motorOnTime(95, 100);
+    motorOnTime(94, 100);
+    motorOnTime2(83, 93, 100);
+    motorOnTime2(72, 82, 100);
+    motorOnTime(61, 100);
+    motorOnTime(51, 100);
+    motorOnTime(41, 100);
+    motorOnTime(31, 100);
+    motorOnTime2(12, 22, 100);
+}
+
+void letraP(){
+    motorOnTime(8, 100);
+    motorOnTime(18, 100);
+    motorOnTime(28, 100);
+    motorOnTime(38, 100);
+    motorOnTime(48, 100);
+    motorOnTime(58, 100);
+    motorOnTime(68, 100);
+    motorOnTime(78, 100);
+    motorOnTime(88, 100);
+    motorOnTime(98, 100);
+
+    motorOnTime(8, 100);
+    motorOnTime(7, 100);
+    motorOnTime(6, 100);
+    motorOnTime2(5, 15, 100);
+    motorOnTime(24, 100);
+    motorOnTime(34, 100);
+    motorOnTime2(44, 45, 100);
+    motorOnTime(56, 100);
+    motorOnTime(57, 100);
+    motorOnTime(58, 100);
+}
+
+void letraQ(){
+    motorOnTime(12, 100);
+    motorOnTime2(3, 13, 100);
+    motorOnTime(4, 100);
+    motorOnTime(5, 100);
+    motorOnTime(6, 100);
+    motorOnTime(7, 100);
+    motorOnTime2(8, 18, 100);
+    motorOnTime2(19, 29, 100);
+    motorOnTime(40, 100);
+    motorOnTime(50, 100);
+    motorOnTime(60, 100);
+    motorOnTime(70, 100);
+    motorOnTime2(79, 89, 100);
+    motorOnTime2(88, 98, 100);
+    motorOnTime(87, 100);
+    motorOnTime(96, 100);
+    motorOnTime(95, 100);
+    motorOnTime(94, 100);
+    motorOnTime2(83, 93, 100);
+    motorOnTime2(72, 82, 100);
+    motorOnTime(61, 100);
+    motorOnTime(51, 100);
+    motorOnTime(41, 100);
+    motorOnTime(31, 100);
+    motorOnTime2(12, 22, 100);
+
+    motorOnTime(64, 100);
+    motorOnTime(73, 100);
+    motorOnTime(82, 100);
+    motorOnTime(91, 100);
+}
+
+void letraR(){
+    motorOnTime(8, 100);
+    motorOnTime(18, 100);
+    motorOnTime(28, 100);
+    motorOnTime(38, 100);
+    motorOnTime(48, 100);
+    motorOnTime(58, 100);
+    motorOnTime(68, 100);
+    motorOnTime(78, 100);
+    motorOnTime(88, 100);
+    motorOnTime(98, 100);
+
+    motorOnTime(8, 100);
+    motorOnTime(7, 100);
+    motorOnTime(6, 100);
+    motorOnTime2(5, 15, 100);
+    motorOnTime(24, 100);
+    motorOnTime(34, 100);
+    motorOnTime2(44, 45, 100);
+    motorOnTime(56, 100);
+    motorOnTime(57, 100);
+    motorOnTime(58, 100);
+
+    motorOnTime(57, 100);
+    motorOnTime(66, 100);
+    motorOnTime(75, 100);
+    motorOnTime(84, 100);
+    motorOnTime(93, 100);
+
+}
+
+void letraS(){
+
+    motorOnTime(13, 100);
+    motorOnTime(4, 100);
+    motorOnTime(5, 100);
+    motorOnTime2(6, 16, 100);
+    motorOnTime(17, 100);
+    motorOnTime(27, 100);
+    motorOnTime(37, 100);
+    motorOnTime2(36, 46, 100);
+    motorOnTime(45, 100);
+    motorOnTime2(44, 54, 100);
+    motorOnTime2(53, 54, 100);
+    motorOnTime(63, 100);
+    motorOnTime(73, 100);
+    motorOnTime2(83, 84, 100);
+    motorOnTime2(94, 95, 100);
+    motorOnTime(96, 100);
+    motorOnTime2(87, 97, 100);
+    motorOnTime2(87, 88, 100);
+    motorOnTime(78, 100);
+
+
+
+}
+
+void letraT(){
+    motorOnTime2(8, 18, 100);
+    motorOnTime2(7, 17, 100);
+    motorOnTime2(6, 16, 100);
+    motorOnTime2(5, 15, 100);
+    motorOnTime2(4, 14, 100);
+    motorOnTime2(3, 13, 100);
+
+    motorOnTime2(15, 16, 100);
+    motorOnTime2(25, 26, 100);
+    motorOnTime2(35, 36, 100);
+    motorOnTime2(45, 46, 100);
+    motorOnTime2(55, 56, 100);
+    motorOnTime2(65, 66, 100);
+    motorOnTime2(75, 76, 100);
+    motorOnTime2(85, 86, 100);
+    motorOnTime2(95, 96, 100);
+}
+
+void letraU(){
+    motorOnTime(8, 100);
+    motorOnTime(18, 100);
+    motorOnTime(28, 100);
+    motorOnTime(38, 100);
+    motorOnTime(48, 100);
+    motorOnTime(58, 100);
+    motorOnTime(68, 100);
+    motorOnTime(78, 100);
+
+    motorOnTime2(87, 88, 100);
+    motorOnTime(96, 100);
+    motorOnTime(95, 100);
+    motorOnTime2(83, 84, 100);
+
+    motorOnTime(73, 100);
+    motorOnTime(63, 100);
+    motorOnTime(53, 100);
+    motorOnTime(43, 100);
+    motorOnTime(33, 100);
+    motorOnTime(23, 100);
+    motorOnTime(13, 100);
+    motorOnTime(3, 100);
+
+
+}
+
+void letraV(){
+    motorOnTime(9, 100);
+    motorOnTime2(18, 19, 100);
+    motorOnTime(28, 100);
+    motorOnTime2(38, 100, 100);
+    motorOnTime2(47, 48, 100);
+    motorOnTime(57, 100);
+    motorOnTime2(66, 67, 100);
+    motorOnTime(76, 100);
+    motorOnTime2(85, 86, 100);
+    motorOnTime(75, 100);
+    motorOnTime2(64, 65, 100);
+    motorOnTime(54, 100);
+    motorOnTime2(43, 44, 100);
+    motorOnTime(33, 100);
+    motorOnTime(23, 100);
+    motorOnTime2(12, 13, 100);
+    motorOnTime(2, 100);
+}
+
+void letraW(){
+    motorOnTime(20, 100);
+    motorOnTime2(29, 30, 100);
+    motorOnTime(39, 100);
+    motorOnTime(49, 100);
+    motorOnTime2(58, 59, 100);
+    motorOnTime(68, 100);
+    motorOnTime(78, 100);
+    motorOnTime2(87, 88, 100);
+    motorOnTime(77, 100);
+    motorOnTime(67, 100);
+    motorOnTime2(56, 57, 100);
+    motorOnTime2(46, 47, 100);
+    motorOnTime(36, 100);
+    motorOnTime(26, 100);
+    motorOnTime2(15, 16, 100);
+    motorOnTime(25, 100);
+    motorOnTime(35, 100);
+    motorOnTime2(44, 45, 100);
+    motorOnTime2(54, 55, 100);
+    motorOnTime(64, 100);
+    motorOnTime(74, 100);
+    motorOnTime2(83, 84, 100);
+    motorOnTime(73, 100);
+    motorOnTime(63, 100);
+    motorOnTime2(52, 53, 100);
+    motorOnTime(42, 100);
+    motorOnTime(32, 100);
+    motorOnTime2(22, 22, 100);
+    motorOnTime(11, 100);
+
+}
+
+void letraX(){
+    motorOnTime(8, 100);
+    motorOnTime2(17, 18, 100);
+    motorOnTime(27, 100);
+    motorOnTime2(36, 37, 100);
+    motorOnTime(46, 100);
+    motorOnTime(55, 100);
+    motorOnTime2(64, 65, 100);
+    motorOnTime(74, 100);
+    motorOnTime2(83, 84, 100);
+    motorOnTime(93, 100);
+
+    motorOnTime(3, 100);
+    motorOnTime2(13, 14, 100);
+    motorOnTime(24, 100);
+    motorOnTime2(34, 35, 100);
+    motorOnTime(45, 100);
+    motorOnTime(56, 100);
+    motorOnTime2(66, 67, 100);
+    motorOnTime(77, 100);
+    motorOnTime2(87, 88, 100);
+    motorOnTime(98, 100);
+
+}
+
+void letraY(){
+    motorOnTime(8, 100);
+    motorOnTime2(17, 18, 100);
+    motorOnTime(27, 100);
+    motorOnTime2(36, 37, 100);
+    motorOnTime(46, 100);
+
+
+
+    motorOnTime(3, 100);
+    motorOnTime2(13, 14, 100);
+    motorOnTime(24, 100);
+    motorOnTime2(34, 35, 100);
+    motorOnTime(45, 100);
+    motorOnTime(56, 100);
+    motorOnTime2(66, 67, 100);
+    motorOnTime(77, 100);
+    motorOnTime2(87, 88, 100);
+    motorOnTime(98, 100);
+
+}
+
+void letraZ(){
+    motorOnTime(8, 100);
+    motorOnTime(7, 100);
+    motorOnTime(6, 100);
+    motorOnTime(5, 100);
+    motorOnTime(4, 100);
+    motorOnTime(3, 100);
+
+    motorOnTime(3, 100);
+    motorOnTime2(13, 14, 100);
+    motorOnTime(24, 100);
+    motorOnTime2(34, 35, 100);
+    motorOnTime(45, 100);
+    motorOnTime(56, 100);
+    motorOnTime2(66, 67, 100);
+    motorOnTime(77, 100);
+    motorOnTime2(87, 88, 100);
+    motorOnTime(98, 100);
+
+    motorOnTime(98, 100);
+    motorOnTime(97, 100);
+    motorOnTime(96, 100);
+    motorOnTime(95, 100);
+    motorOnTime(94, 100);
+    motorOnTime(93, 100);
+}
+
+void letra(char l){
+    switch(l){
+    case 'A':
+        letraA();
+    break;
+    case 'B':
+        letraB();
+    break;
+    case 'C':
+        letraC();
+    break;
+    case 'D':
+        letraD();
+    break;
+    case 'E':
+        letraE();
+    break;
+    case 'F':
+        letraF();
+    break;
+    case 'G':
+        letraG();
+    break;
+    case 'H':
+        letraH();
+    break;
+    case 'I':
+        letraI();
+    break;
+    case 'J':
+        letraJ();
+    break;
+    case 'K':
+        letraK();
+    break;
+    case 'L':
+        letraL();
+    break;
+    case 'M':
+        letraM();
+    break;
+    case 'N':
+        letraN();
+    break;
+    case 'O':
+        letraO();
+    break;
+    case 'P':
+        letraP();
+    break;
+    case 'Q':
+        letraQ();
+    break;
+    case 'R':
+        letraR();
+    break;
+    case 'S':
+        letraS();
+    break;
+    case 'T':
+        letraT();
+    break;
+    case 'U':
+        letraU();
+    break;
+    case 'V':
+        letraV();
+    break;
+    case 'W':
+        letraW();
+    break;
+    case 'X':
+        letraX();
+    break;
+    case 'Y':
+        letraY();
+    break;
+    case 'Z':
+        letraZ();
+    break;
+    default:
+        gpioDelay(500000);
+    break;
+    }
+}
